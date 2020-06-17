a5d0861f713f56a5f8b148e2b11caf8f0cffda20
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-17 21:13:50 +0000

    Deploy Chart-Covid/frontend to github.com/Chart-Covid/frontend.git:gh-pages

---
+++ precache-manifest.e2f8cece0c40596469a13eef905f0e02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5012db6bb7a442f0b417156c1d9eed44",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "394f1678255524f1584f",
+    "url": "/frontend/static/css/main.c80f8384.chunk.css"
+  },
+  {
+    "revision": "9fd84bf897b618a176ab",
+    "url": "/frontend/static/js/2.6e29a2ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend/static/js/2.6e29a2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394f1678255524f1584f",
+    "url": "/frontend/static/js/main.b48946d8.chunk.js"
+  },
+  {
+    "revision": "bb9cf7b820440dcc0565",
+    "url": "/frontend/static/js/runtime-main.e2bed40b.js"
+  }
+]);
