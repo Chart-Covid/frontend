0645d0b740dd6c8dff1f9414a0d92659f2ed017a
Beto <betotoro0902@gmail.com>
2020-06-17 16:59:50 -0500

    <Feature>: Carpeta 'scss' => archivo 'Variables.scss'

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/index.scss';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import App from './App';
+
+import './scss/Variables.scss';
 
 ReactDOM.render(
   <React.StrictMode>
