730b28b6d3fea306b5e346b46f4e7620c78fecc0
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-06-17 08:20:58 -0500

    <Feature>: Base of project

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Example } from "../components/Example";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Chart-Covid</h1>
+      <Example />
+    </div>
+  );
+};
