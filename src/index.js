df7930f7247f5ccfbb7684b99c087634a4161500
Erickowski <daniel_perez_03081995@hotmail.com>
2020-06-25 14:04:20 -0500

    Cambia pais cada que das clic en un marcador

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import Context from './Context';
 
 import App from './App';
 
 import './scss/Variables.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Context.Provider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Context.Provider>,
   document.getElementById('root')
 );
 
