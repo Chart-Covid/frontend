f4320e1b5bb8a1957bb1d89ab8f7224e1deb10cc
Erickowski <daniel_perez_03081995@hotmail.com>
2020-06-25 10:40:20 -0500

    Opacidad en 1 significa que es del top

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Map, TileLayer, Popup, Marker } from "react-leaflet";
 
-
 import "./styles.scss";
 
-const MapContainer = ({ Datos }) => {
+const MapContainer = ({ Datos, topCountries={} }) => {
   return (
     <Map center={[0, 0]} zoom={2} keyboard={false} zoomControl={false}>
       <TileLayer
@@ -12,9 +11,11 @@ const MapContainer = ({ Datos }) => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       {Datos.map((dato, index) => {
+        let opacity;
+        (Object.keys(topCountries).includes(dato.name)) ? opacity = 1 : opacity = 0.5;
         let positionCountrie = [dato.location.lat, dato.location.lng];
         return (
-          <Marker key={index} position={positionCountrie}>
+          <Marker key={index} position={positionCountrie} opacity={opacity}>
             <Popup>
               Pais: {dato.name}
               <br />
